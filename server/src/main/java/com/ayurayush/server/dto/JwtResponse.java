206d9837be1952843587f40caa677b741291047e
Aravind Murali <aravind_murali@bio-rad.com>
2025-08-07 00:57:45 -0400

    Squashed 'backend/' content from commit e396aad
    git-subtree-dir: backend
    git-subtree-split: e396aadfdc2555309d06af5df0cc180c04493bed

---
+++ server/src/main/java/com/ayurayush/server/dto/JwtResponse.java
@@ -0,0 +1,42 @@
+package com.ayurayush.server.dto;
+
+public class JwtResponse {
+    private String token;
+    private String username;
+    private String role;
+    private Long userId;
+
+    public JwtResponse() {}
+
+    public JwtResponse(String token, String username, String role, Long userId) {
+        this.token = token;
+        this.username = username;
+        this.role = role;
+        this.userId = userId;
+    }
+
+    public String getToken() {
+        return token;
+    }
+    public void setToken(String token) {
+        this.token = token;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getRole() {
+        return role;
+    }
+    public void setRole(String role) {
+        this.role = role;
+    }
+    public Long getUserId() {
+        return userId;
+    }
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+}
